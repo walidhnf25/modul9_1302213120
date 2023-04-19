0c84e3d228ffdb6e8b4d6fe50cc642fbfedd1dae
walidhnf25 <109956289+walidhnf25@users.noreply.github.com>
2023-04-19 19:54:44 +0700

    Update WeatherForecastController.cs

--- Modul9_13022131200/Controllers/WeatherForecastController.cs
+++ Modul9_13022131200/Controllers/WeatherForecastController.cs
@@ -1,6 +1,33 @@
-﻿namespace Modul9_13022131200.Controllers
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modul9_13022131200.Controllers
 {
-    public class WeatherForecastController
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
     {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
